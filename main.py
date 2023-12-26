294c00ce305a13f9c46e2ef58bba01e1d53bc8b8
hinsoned <102074110+hinsoned@users.noreply.github.com>
2023-12-25 20:22:18 -0500

    Adding ship variables

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+#This imports operating system which allows you to define the path to the images folder
+import os
 
 # print("Hello world")
 
@@ -11,8 +13,14 @@ WHITE = (255, 255, 255)
 #This is to control how quickly the game updates the screen
 FPS = 60
 
-def draw_window():
+#Import images of ships
+YELLOW_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assests', 'spaceship_yellow.png'))
+RED_SPACESHIP_IMAGE = pygame.image.load(os.path.join('Assests', 'spaceship_red.png'))
+
+def draw_window(): 
+    
     WIN.fill(WHITE)
+    WIN.blit()
     pygame.display.update()
 
 def main():
